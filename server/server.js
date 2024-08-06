460737305b1a56d134d9666b3e802580111b18ed
RupaliBanerjee <rupali.banerjee024@gmail.com>
2024-08-07 00:10:28 +0100

    index.html path update

--- server/server.js
+++ server/server.js
@@ -174,7 +174,7 @@ app.post("/api/updateItems", async (req, res) => {
 
 // Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 app.listen(port, () => {
